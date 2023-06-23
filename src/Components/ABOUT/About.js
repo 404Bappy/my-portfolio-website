5a3bd55870862b69fb9d531b5b39a027148ade7a
Bappy71 <19103076@iubat.edu>
2023-06-23 09:11:41 +0600

    first commit

--- src/Components/ABOUT/About.js
+++ src/Components/ABOUT/About.js
@@ -1,10 +1,44 @@
 import React from "react";
 import "./About.css";
+import meImg from "../../Assets/meme.png";
+import { FaAward } from "react-icons/fa";
+import { LiaUsersCogSolid } from "react-icons/lia";
+import { AiOutlineFolderAdd } from "react-icons/ai";
 
 const About = () => {
-  return(
-    <section id="about">ABOUT</section>
-  ) 
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="aboutme-img">
+            <img src={meImg} alt="" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about-card">
+            <article className="a-card">
+              <FaAward className="aboutIcon" />
+              <h5>Experience</h5>
+              <small>1+ Years Working Experience</small>
+            </article>
+            <article className="a-card">
+              <LiaUsersCogSolid className="aboutIcon" />
+              <h5>Clients</h5>
+              <small>20+</small>
+            </article>
+            <article className="a-card">
+              <AiOutlineFolderAdd className="aboutIcon" />
+              <h5>Projects</h5>
+              <small>60+ Completed Projects</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/Components/NAV_BAR/Navbar.js
+++ src/Components/NAV_BAR/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { BiSolidUserCheck } from "react-icons/bi";
@@ -7,21 +7,42 @@ import { RiServiceLine } from "react-icons/ri";
 import { AiOutlineMessage } from "react-icons/ai";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <BiSolidUserCheck />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <AiOutlineMessage />
       </a>
     </nav>
